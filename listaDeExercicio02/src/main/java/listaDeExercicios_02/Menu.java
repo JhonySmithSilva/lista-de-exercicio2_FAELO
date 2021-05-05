eb8bceda03f21e07680fdc54020a475006c2bcd9
Jhony Smith <61160773+JhonySmithSilva@users.noreply.github.com>
2021-05-05 13:32:23 -0300

    criando a classe menu

--- listaDeExercicio02/src/main/java/listaDeExercicios_02/Menu.java
+++ listaDeExercicio02/src/main/java/listaDeExercicios_02/Menu.java
@@ -1,21 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 
  */
 package listaDeExercicios_02;
-
+import java.util.Scanner;
 /**
  *
  * @author jony_
  */
 public class Menu {
 
-    /**
-     * @param args the command line arguments
-     */
+    private int opcao;    
+    Scanner input = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
