e79af0d9b59163fa27e170646657d98c3f91e8a7
jony_ <jony_@DESKTOP-B7MOECE>
2021-05-12 00:32:53 -0300

    alterações nas classes de exercicios

--- listaDeExercicio02/src/main/java/listaDeExercicios_02/Menu.java
+++ listaDeExercicio02/src/main/java/listaDeExercicios_02/Menu.java
@@ -41,31 +41,25 @@ public class Menu {
             this.opcoes = input.nextInt();
 
             switch(opcoes){
-                case 1: {
+                case 1 ->  {
                     System.out.print("  digite o elemento a ser adicionado:  ");
                     lista.adicionar(input.nextInt());
-                    break;
                 }
-                case 2: {
+                case 2 ->  {
                     lista.removerInicio();
-                    break;
                 }
-                case 3: {
+                case 3 ->  {
                     lista.remover(input.nextInt());
-                    break;
                 }
-                case 4: {
+                case 4 ->  {
                     lista.estaVazio();
-                    break;
                 }
-                case 5: {
+                case 5 ->  {
                     System.out.println("digite o numero que deseja pesquisar: ");
                     lista.pesquisar(input.nextInt());
-                    break;
                 }
-                case 6: {                    
+                case 6 ->  {                    
                     lista.toString();
-                    break;
                 }                         
             }
             if(this.opcoes < 0 && this.opcoes > 6 ){
