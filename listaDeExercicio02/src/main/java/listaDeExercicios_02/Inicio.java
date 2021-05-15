7a32f49ec5e2183106d5e3110202bca3fbf613b6
jony_ <jony_@DESKTOP-B7MOECE>
2021-05-15 00:46:44 -0300

    alterações de teste

--- listaDeExercicio02/src/main/java/listaDeExercicios_02/Inicio.java
+++ listaDeExercicio02/src/main/java/listaDeExercicios_02/Inicio.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package listaDeExercicios_02;
 
 /**
@@ -11,9 +7,7 @@ package listaDeExercicios_02;
  */
 public class Inicio {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Menu menu = new Menu();
         
